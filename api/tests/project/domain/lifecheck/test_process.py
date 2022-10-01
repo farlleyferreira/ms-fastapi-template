1fa4081ecdb0e395f2f452694894c94f7d90811e
giovannifarlley <farlley@live.com>
2022-10-01 00:17:21 -0300

    fix: otimização template

--- api/tests/project/domain/lifecheck/test_process.py
+++ api/tests/project/domain/lifecheck/test_process.py
@@ -6,10 +6,10 @@ from project.infrastructure.constants.health_check_status import Health
 def test_get_api_status_danger():
     list_status = [Status.RED, Status.RED, Status.RED, Status.RED, ]
     api_status = ValidateHelth().validate_general_status(list_status)
-    assert api_status == Health.danger.value
+    assert api_status == "danger"
 
 
 def test_get_api_status_warning():
     list_status = [Status.GREEN, Status.RED, Status.RED, Status.RED, ]
     api_status = ValidateHelth().validate_general_status(list_status)
-    assert api_status == Health.warning.value
+    assert api_status == "warning"
